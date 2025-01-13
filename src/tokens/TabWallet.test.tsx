8bb398a20b151e1718bded1bef493c25d393ebd0
jh2oman <jacobrwaterman@gmail.com>
2025-01-13 12:13:09 -0800

    back to valora wallet style

--- src/tokens/TabWallet.test.tsx
+++ src/tokens/TabWallet.test.tsx
@@ -1,53 +1,88 @@
 import { fireEvent, render } from '@testing-library/react-native'
-import React from 'react'
+import * as React from 'react'
 import { Provider } from 'react-redux'
 import { navigate } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
 import { getFeatureGate, getMultichainFeatures } from 'src/statsig'
+import { StatsigFeatureGates } from 'src/statsig/types'
 import TabWallet from 'src/tokens/TabWallet'
 import { NetworkId } from 'src/transactions/types'
 import MockedNavigator from 'test/MockedNavigator'
 import { createMockStore } from 'test/utils'
-import { mockCeurAddress, mockCeurTokenId, mockCusdAddress, mockCusdTokenId } from 'test/values'
+import {
+  mockCeurAddress,
+  mockCeurTokenId,
+  mockCusdAddress,
+  mockCusdTokenId,
+  mockPositions,
+  mockShortcuts,
+} from 'test/values'
 
 jest.mock('src/statsig')
 
-function getStore({ zeroBalance = false }: { zeroBalance?: boolean } = {}) {
-  return {
-    tokens: {
-      tokenBalances: {
-        [mockCeurTokenId]: {
-          tokenId: mockCeurTokenId,
-          priceUsd: '1.16',
-          address: mockCeurAddress,
-          symbol: 'cEUR',
-          imageUrl:
-            'https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_cEUR.png',
-          name: 'Celo Euro',
-          decimals: 18,
-          balance: zeroBalance ? '0' : '5',
-          isFeeCurrency: true,
-          canTransferWithComment: true,
-          priceFetchedAt: Date.now(),
-          networkId: NetworkId['celo-alfajores'],
-        },
-        [mockCusdTokenId]: {
-          tokenId: mockCusdTokenId,
-          priceUsd: '1.001',
-          address: mockCusdAddress,
-          symbol: 'cUSD',
-          imageUrl:
-            'https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_cUSD.png',
-          name: 'Celo Dollar',
-          decimals: 18,
-          balance: zeroBalance ? '0' : '10',
-          isFeeCurrency: true,
-          canTransferWithComment: true,
-          priceFetchedAt: Date.now(),
-          networkId: NetworkId['celo-alfajores'],
-        },
+const storeWithTokenBalances = {
+  tokens: {
+    tokenBalances: {
+      [mockCeurTokenId]: {
+        tokenId: mockCeurTokenId,
+        priceUsd: '1.16',
+        address: mockCeurAddress,
+        symbol: 'cEUR',
+        imageUrl:
+          'https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_cEUR.png',
+        name: 'Celo Euro',
+        decimals: 18,
+        balance: '5',
+        isFeeCurrency: true,
+        canTransferWithComment: true,
+        priceFetchedAt: Date.now(),
+        networkId: NetworkId['celo-alfajores'],
+      },
+      [mockCusdTokenId]: {
+        tokenId: mockCusdTokenId,
+        priceUsd: '1.001',
+        address: mockCusdAddress,
+        symbol: 'cUSD',
+        imageUrl:
+          'https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_cUSD.png',
+        name: 'Celo Dollar',
+        decimals: 18,
+        balance: '10',
+        isFeeCurrency: true,
+        canTransferWithComment: true,
+        priceFetchedAt: Date.now(),
+        networkId: NetworkId['celo-alfajores'],
       },
     },
-  }
+  },
+  positions: {
+    positions: [],
+  },
+}
+
+const storeWithZeroTokenBalances = {
+  tokens: {
+    tokenBalances: {},
+  },
+  positions: {
+    positions: [],
+  },
+}
+
+const storeWithPositions = {
+  ...storeWithTokenBalances,
+  positions: {
+    positions: mockPositions, // Total value of positions is ~$7.91 or ~₱10.52
+    shortcuts: [],
+  },
+}
+
+const storeWithPositionsAndClaimableRewards = {
+  ...storeWithTokenBalances,
+  positions: {
+    positions: mockPositions,
+    shortcuts: mockShortcuts,
+  },
 }
 
 describe('TabWallet', () => {
@@ -58,43 +93,147 @@ describe('TabWallet', () => {
       showBalances: [NetworkId['celo-alfajores']],
     })
   })
-  it('shows correct total balance, cEUR and cUSD line items when tokens have balance', () => {
-    const { getByTestId } = render(
-      <Provider store={createMockStore(getStore())}>
-        <MockedNavigator component={TabWallet} />
-      </Provider>
-    )
-    expect(getByTestId('TotalTokenBalance')).toHaveTextContent('₱21.03')
-    expect(getByTestId('cEURBalance')).toHaveTextContent('5.00 cEUR')
-    expect(getByTestId('cUSDBalance')).toHaveTextContent('10.00 cUSD')
 
-    expect(getByTestId('BuyTokens')).toBeTruthy()
-    fireEvent.press(getByTestId('BuyTokens'))
-    expect(navigate).toHaveBeenCalledWith('FiatExchangeCurrencyBottomSheet', {
-      flow: 'CashIn',
-    })
-  })
-  it('shows cEUR and cUSD line items when tokens do not have balance', () => {
-    const { getByTestId, getByText } = render(
-      <Provider store={createMockStore(getStore({ zeroBalance: true }))}>
+  it('renders the zero state if there are no tokens', () => {
+    const store = createMockStore(storeWithZeroTokenBalances)
+
+    const { getByText } = render(
+      <Provider store={store}>
         <MockedNavigator component={TabWallet} />
       </Provider>
     )
+
     expect(getByText('assets.noTokensTitle')).toBeTruthy()
-    expect(getByText('assets.noTokensDescription')).toBeTruthy()
-    expect(getByTestId('ZeroStateBuyTokens')).toBeTruthy()
-    fireEvent.press(getByTestId('ZeroStateBuyTokens'))
-    expect(navigate).toHaveBeenCalledWith('FiatExchangeCurrencyBottomSheet', {
-      flow: 'CashIn',
-    })
   })
-  it('tapping asset navigates to TokenDetails screen', () => {
-    const { getByTestId } = render(
-      <Provider store={createMockStore(getStore())}>
+
+  it('renders tokens and dapp positions tabs', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithPositions)
+
+    const { getByTestId, getAllByTestId, queryByTestId, queryAllByTestId, getByText, queryByText } =
+      render(
+        <Provider store={store}>
+          <MockedNavigator component={TabWallet} />
+        </Provider>
+      )
+
+    expect(getByTestId('AssetsTokenBalance')).toBeTruthy()
+    expect(queryByTestId('AssetsTokenBalance/Info')).toBeFalsy()
+    expect(getByTestId('AssetsTokenBalance')).toHaveTextContent('₱31.55')
+
+    expect(getByTestId('Assets/TabBar')).toBeTruthy()
+    expect(getAllByTestId('Assets/TabBarItem')).toHaveLength(2)
+    expect(getByText('assets.tabBar.tokens')).toBeTruthy()
+    expect(queryByText('assets.tabBar.dappPositions')).toBeTruthy()
+
+    expect(getAllByTestId('TokenBalanceItem')).toHaveLength(2)
+    expect(queryAllByTestId('PositionItem')).toHaveLength(0)
+  })
+
+  it('hides dapp positions if feature gate is enabled but there are no positions', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithTokenBalances)
+
+    const { getByTestId, getAllByTestId, queryAllByTestId, getByText, queryByTestId, queryByText } =
+      render(
+        <Provider store={store}>
+          <MockedNavigator component={TabWallet} />
+        </Provider>
+      )
+
+    expect(getByTestId('AssetsTokenBalance')).toBeTruthy()
+    expect(queryByTestId('AssetsTokenBalance/Info')).toBeFalsy()
+    expect(getByTestId('AssetsTokenBalance')).toHaveTextContent('₱21.03')
+
+    expect(getByTestId('Assets/TabBar')).toBeTruthy()
+    expect(getAllByTestId('Assets/TabBarItem')).toHaveLength(1)
+    expect(getByText('assets.tabBar.tokens')).toBeTruthy()
+    expect(queryByText('assets.tabBar.dappPositions')).toBeFalsy()
+
+    expect(getAllByTestId('TokenBalanceItem')).toHaveLength(2)
+    expect(queryAllByTestId('PositionItem')).toHaveLength(0)
+  })
+
+  it('renders dapp positions on selecting the tab', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithPositions)
+
+    const { getAllByTestId, queryAllByTestId, getByText } = render(
+      <Provider store={store}>
         <MockedNavigator component={TabWallet} />
       </Provider>
     )
-    fireEvent.press(getByTestId('cEURSymbol'))
-    expect(navigate).toHaveBeenCalledWith('TokenDetails', { tokenId: mockCeurTokenId })
+
+    expect(getAllByTestId('TokenBalanceItem')).toHaveLength(2)
+    expect(queryAllByTestId('PositionItem')).toHaveLength(0)
+
+    fireEvent.press(getByText('assets.tabBar.dappPositions'))
+
+    expect(getAllByTestId('PositionItem')).toHaveLength(3)
+    expect(queryAllByTestId('TokenBalanceItem')).toHaveLength(0)
+
+    fireEvent.press(getByText('assets.tabBar.tokens'))
+
+    expect(getAllByTestId('TokenBalanceItem')).toHaveLength(2)
+    expect(queryAllByTestId('PositionItem')).toHaveLength(0)
+  })
+
+  it('hides claim rewards if feature gate is false', () => {
+    jest
+      .mocked(getFeatureGate)
+      .mockImplementation((gate) => gate === StatsigFeatureGates.SHOW_POSITIONS)
+    const store = createMockStore(storeWithPositionsAndClaimableRewards)
+
+    const { queryByText } = render(
+      <Provider store={store}>
+        <MockedNavigator component={TabWallet} />
+      </Provider>
+    )
+
+    expect(queryByText('assets.claimRewards')).toBeFalsy()
+  })
+
+  it('hides claim rewards if feature gate is true and positions do not include claimable rewards', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithPositions)
+
+    const { queryByText } = render(
+      <Provider store={store}>
+        <MockedNavigator component={TabWallet} />
+      </Provider>
+    )
+
+    expect(queryByText('assets.claimRewards')).toBeFalsy()
+  })
+
+  it('shows claim rewards if feature gate is true and positions include claimable rewards', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithPositionsAndClaimableRewards)
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <MockedNavigator component={TabWallet} />
+      </Provider>
+    )
+
+    expect(getByText('assets.claimRewards')).toBeTruthy()
+    fireEvent.press(getByText('assets.tabBar.dappPositions'))
+    expect(getByText('assets.claimRewards')).toBeTruthy()
+    fireEvent.press(getByText('assets.tabBar.tokens'))
+    expect(getByText('assets.claimRewards')).toBeTruthy()
+  })
+
+  it('clicking claim rewards navigates to rewards screen', () => {
+    jest.mocked(getFeatureGate).mockReturnValue(true)
+    const store = createMockStore(storeWithPositionsAndClaimableRewards)
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <MockedNavigator component={TabWallet} />
+      </Provider>
+    )
+
+    fireEvent.press(getByText('assets.claimRewards'))
+    expect(navigate).toHaveBeenCalledWith(Screens.DappShortcutsRewards)
   })
 })
--- src/tokens/TabWallet.tsx
+++ src/tokens/TabWallet.tsx
@@ -1,37 +1,165 @@
-import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { getNumberFormatSettings } from 'react-native-localize'
+import { Image, LayoutChangeEvent, StyleSheet, Text, View } from 'react-native'
+import Animated, {
+  interpolateColor,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
 import AppAnalytics from 'src/analytics/AppAnalytics'
 import { AssetsEvents } from 'src/analytics/Events'
-import { hideWalletBalancesSelector } from 'src/app/selectors'
 import Button, { BtnSizes, BtnTypes } from 'src/components/Button'
-import { HideBalanceButton } from 'src/components/TokenBalance'
+import { AssetsTokenBalance } from 'src/components/TokenBalance'
 import { FiatExchangeFlow } from 'src/fiatExchanges/types'
+import ActionsCarousel from 'src/home/ActionsCarousel'
 import { cowSpaceship } from 'src/images/Images'
-import { getLocalCurrencySymbol } from 'src/localCurrency/selectors'
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
+import useScrollAwareHeader from 'src/navigator/ScrollAwareHeader'
+import { StackParamList } from 'src/navigator/types'
+import {
+  positionsWithBalanceSelector,
+  positionsWithClaimableRewardsSelector,
+} from 'src/positions/selectors'
 import { useSelector } from 'src/redux/hooks'
+import { getFeatureGate } from 'src/statsig'
+import { StatsigFeatureGates } from 'src/statsig/types'
+import Colors from 'src/styles/colors'
 import { typeScale } from 'src/styles/fonts'
-import { Spacing } from 'src/styles/styles'
-import { useTotalTokenBalance } from 'src/tokens/hooks'
+import { Shadow, Spacing, getShadowStyle } from 'src/styles/styles'
+import AssetList from 'src/tokens/AssetList'
+import AssetTabBar from 'src/tokens/AssetTabBar'
 import { sortedTokensWithBalanceSelector } from 'src/tokens/selectors'
-import { TokenBalanceItem } from 'src/tokens/TokenBalanceItem'
-import { getSupportedNetworkIdsForTokenBalances, getTokenAnalyticsProps } from 'src/tokens/utils'
+import { AssetTabType } from 'src/tokens/types'
+import { getSupportedNetworkIdsForTokenBalances } from 'src/tokens/utils'
 
-function TabWallet() {
+type Props = NativeStackScreenProps<StackParamList, Screens.TabWallet>
+
+// offset relative to the bottom of the non sticky header component, where the
+// screen header opacity animation starts
+const HEADER_OPACITY_ANIMATION_START_OFFSET = 44
+// distance in points over which the screen header opacity animation is applied
+const HEADER_OPACITY_ANIMATION_DISTANCE = 20
+
+function TabWallet({ navigation, route }: Props) {
   const { t } = useTranslation()
-  const hideWalletBalances = useSelector(hideWalletBalancesSelector)
-  const localCurrencySymbol = useSelector(getLocalCurrencySymbol)
-  const { decimalSeparator } = getNumberFormatSettings()
+
+  const activeTab = route.params?.activeAssetTab ?? AssetTabType.Tokens
+
+  // TODO: Update this to filter out unsupported networks once positions support non-Celo chains
+  const positions = useSelector(positionsWithBalanceSelector)
+  const showPositions = getFeatureGate(StatsigFeatureGates.SHOW_POSITIONS)
+  const displayPositions = showPositions && positions.length > 0
 
   const supportedNetworkIds = getSupportedNetworkIdsForTokenBalances()
   const tokens = useSelector((state) => sortedTokensWithBalanceSelector(state, supportedNetworkIds))
-  const totalTokenBalanceLocal = useTotalTokenBalance()
-  const balanceDisplay = hideWalletBalances
-    ? `XX${decimalSeparator}XX`
-    : totalTokenBalanceLocal?.toFormat(2)
+
+  const dappShortcutsEnabled = getFeatureGate(StatsigFeatureGates.SHOW_CLAIM_SHORTCUTS)
+  const positionsWithClaimableRewards = useSelector(positionsWithClaimableRewardsSelector)
+  const showClaimRewards =
+    dappShortcutsEnabled &&
+    positionsWithClaimableRewards.length > 0 &&
+    activeTab !== AssetTabType.Collectibles
+
+  const [nonStickyHeaderHeight, setNonStickyHeaderHeight] = useState(0)
+  const [listHeaderHeight, setListHeaderHeight] = useState(0)
+  const [listFooterHeight, setListFooterHeight] = useState(0)
+
+  const scrollPosition = useSharedValue(0)
+  const footerPosition = useSharedValue(0)
+  const handleScroll = useAnimatedScrollHandler<{ prevScrollY: number }>(
+    {
+      onScroll: (event, ctx) => {
+        const scrollY = event.contentOffset.y
+        scrollPosition.value = scrollY
+
+        function clamp(value: number, min: number, max: number) {
+          return Math.min(Math.max(value, min), max)
+        }
+
+        // Omit overscroll in the calculation
+        const clampedScrollY = clamp(
+          scrollY,
+          0,
+          event.contentSize.height - event.layoutMeasurement.height
+        )
+
+        // This does the same as React Native's Animated.diffClamp
+        const diff = clampedScrollY - ctx.prevScrollY
+        footerPosition.value = clamp(footerPosition.value + diff, 0, listFooterHeight)
+        ctx.prevScrollY = clampedScrollY
+      },
+      onBeginDrag: (event, ctx) => {
+        ctx.prevScrollY = event.contentOffset.y
+      },
+    },
+    [listFooterHeight]
+  )
+
+  const animatedListHeaderStyles = useAnimatedStyle(() => {
+    if (nonStickyHeaderHeight === 0) {
+      return {
+        shadowColor: 'transparent',
+        transform: [
+          {
+            translateY: -scrollPosition.value,
+          },
+        ],
+      }
+    }
+
+    return {
+      transform: [
+        {
+          translateY:
+            scrollPosition.value > nonStickyHeaderHeight
+              ? -nonStickyHeaderHeight
+              : -scrollPosition.value,
+        },
+      ],
+      shadowColor: interpolateColor(
+        scrollPosition.value,
+        [nonStickyHeaderHeight - 10, nonStickyHeaderHeight + 10],
+        ['transparent', Colors.blue100]
+      ),
+    }
+  }, [scrollPosition.value, nonStickyHeaderHeight])
+
+  const animatedFooterStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: footerPosition.value,
+        },
+      ],
+    }
+  }, [footerPosition.value])
+
+  useScrollAwareHeader({
+    navigation,
+    title: t('bottomTabsNavigator.wallet.title'),
+    scrollPosition,
+    startFadeInPosition: nonStickyHeaderHeight - HEADER_OPACITY_ANIMATION_START_OFFSET,
+    animationDistance: HEADER_OPACITY_ANIMATION_DISTANCE,
+  })
+
+  const handleMeasureNonStickyHeaderHeight = (event: LayoutChangeEvent) => {
+    setNonStickyHeaderHeight(event.nativeEvent.layout.height)
+  }
+
+  const handleMeasureListHeaderHeight = (event: LayoutChangeEvent) => {
+    setListHeaderHeight(event.nativeEvent.layout.height)
+  }
+
+  const handleMeasureListFooterHeight = (event: LayoutChangeEvent) => {
+    setListFooterHeight(event.nativeEvent.layout.height)
+  }
+
+  const handleChangeActiveView = (selectedTab: AssetTabType) => {
+    navigation.setParams({ activeAssetTab: selectedTab })
+  }
 
   if (tokens.length === 0) {
     return (
@@ -54,50 +182,78 @@ function TabWallet() {
   }
 
   return (
-    <View style={styles.container} testID="TabWallet">
-      <View style={styles.row}>
-        <Text style={styles.totalBalance} testID={'TotalTokenBalance'}>
-          {!hideWalletBalances && localCurrencySymbol}
-          {balanceDisplay}
-        </Text>
-        <HideBalanceButton hideBalance={hideWalletBalances} />
-      </View>
-      <ScrollView contentContainerStyle={styles.contentContainerStyle}>
-        {tokens.map((token, index) => (
-          <TokenBalanceItem
-            containerStyle={{ marginHorizontal: Spacing.Regular16 }}
-            token={token}
-            key={index}
-            onPress={() => {
-              navigate(Screens.TokenDetails, { tokenId: token.tokenId })
-              AppAnalytics.track(AssetsEvents.tap_asset, {
-                ...getTokenAnalyticsProps(token),
-                title: token.symbol,
-                description: token.name,
-                assetType: 'token',
-              })
-            }}
-            hideBalances={hideWalletBalances}
-          />
-        ))}
-        <Button
-          testID="BuyTokens"
-          style={{ ...styles.button, marginBottom: Spacing.XLarge48 }}
-          type={BtnTypes.SECONDARY}
-          size={BtnSizes.FULL}
-          onPress={() => {
-            navigate(Screens.FiatExchangeCurrencyBottomSheet, { flow: FiatExchangeFlow.CashIn })
-          }}
-          text={t('assets.buyTokens')}
+    // Transparency issue on Android present when a fragment is used - Nested Animated.View prevents it
+    <Animated.View>
+      <Animated.View
+        style={[styles.listHeaderContainer, animatedListHeaderStyles]}
+        onLayout={handleMeasureListHeaderHeight}
+      >
+        <View
+          style={[styles.nonStickyHeaderContainer]}
+          onLayout={handleMeasureNonStickyHeaderHeight}
+        >
+          <View style={styles.balanceSection}>
+            <AssetsTokenBalance showInfo={false} />
+          </View>
+          <ActionsCarousel key={'ActionsCarousel'} />
+        </View>
+        <AssetTabBar
+          activeTab={activeTab}
+          onChange={handleChangeActiveView}
+          displayPositions={displayPositions}
         />
-      </ScrollView>
-    </View>
+      </Animated.View>
+
+      <AssetList
+        activeTab={activeTab}
+        listHeaderHeight={listHeaderHeight}
+        handleScroll={handleScroll}
+      />
+      {showClaimRewards && (
+        <Animated.View
+          style={[styles.footerContainer, animatedFooterStyles]}
+          onLayout={handleMeasureListFooterHeight}
+        >
+          <Button
+            type={BtnTypes.SECONDARY}
+            size={BtnSizes.FULL}
+            text={t('assets.claimRewards')}
+            onPress={() => {
+              AppAnalytics.track(AssetsEvents.tap_claim_rewards)
+              navigate(Screens.DappShortcutsRewards)
+            }}
+          />
+        </Animated.View>
+      )}
+    </Animated.View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  listHeaderContainer: {
+    ...getShadowStyle(Shadow.SoftLight),
+    paddingBottom: Spacing.Regular16,
+    paddingHorizontal: Spacing.Regular16,
+    backgroundColor: Colors.darkBlue,
+    position: 'absolute',
+    width: '100%',
+    zIndex: 1,
+  },
+  nonStickyHeaderContainer: {
+    zIndex: 1,
+    paddingBottom: Spacing.Thick24,
+  },
+  footerContainer: {
+    backgroundColor: Colors.darkBlue,
+    position: 'absolute',
+    bottom: 0,
+    left: 10, // so the scroll bar is still visible
+    right: 10,
+    paddingHorizontal: Spacing.Thick24 - 10,
+    paddingVertical: Spacing.Regular16,
+  },
+  balanceSection: {
+    paddingBottom: Spacing.Regular16,
   },
   zeroStateContainer: {
     flex: 1,
@@ -105,17 +261,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: Spacing.Regular16,
   },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: Spacing.Smallest8,
-    marginTop: Spacing.Smallest8,
-  },
-  totalBalance: {
-    ...typeScale.titleLarge,
-  },
-  contentContainerStyle: { marginTop: Spacing.Large32 },
   button: {
     marginTop: Spacing.Regular16,
     marginHorizontal: Spacing.Regular16,
