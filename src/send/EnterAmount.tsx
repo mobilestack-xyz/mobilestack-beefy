4a582e0b45a16f1f92c6285407a9b29c6a6fc334
Satish Ravi <satish.ravi@valoraapp.com>
2025-01-17 12:10:14 -0800

    clean

--- src/send/EnterAmount.tsx
+++ src/send/EnterAmount.tsx
@@ -358,8 +358,6 @@ const styles = StyleSheet.create({
     marginVertical: Spacing.Regular16,
     padding: Spacing.Regular16,
     backgroundColor: Colors.blue100,
-    borderColor: Colors.blue200,
-    borderWidth: 0,
     borderRadius: 12,
     gap: Spacing.Smallest8,
     flexDirection: 'row',
